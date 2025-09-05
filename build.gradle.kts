69ef5117fed343c29461a1f89aa278af5ead82f6
V <reza1385.rb@gmail.com>
2025-09-05 20:45:36 +0330

    progress

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
-plugins {
-    id("com.android.application") version "8.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
-    id("com.google.dagger.hilt.android") version "2.48.1" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10" apply false
-}
-
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     repositories {
         google()
         mavenCentral()
+        gradlePluginPortal()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:9.0.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48.1")
     }
 }
 
